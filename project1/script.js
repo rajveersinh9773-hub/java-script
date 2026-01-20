7b6efe76d11bed2190d2136d866dba0d289a22f4
rajveersinh jadeja <rajveersinh9773@gmail.com>
2026-01-20 16:29:14 +0530

    Add files via upload

---
+++ project1/script.js
@@ -0,0 +1,41 @@
+// 1. Print "Hello World"
+console.log("Hello World");
+
+// 2. Add two numbers
+let a = 5;
+let b = 3;
+console.log("Sum:", a + b);
+
+// 3. Subtract two numbers
+console.log("Difference:", a - b);
+
+// 4. Multiply two numbers
+console.log("Product:", a * b);
+
+// 5. Divide two numbers
+console.log("Division:", a / b);
+
+// 6. Find remainder
+console.log("Remainder:", a % b);
+
+// 7. Check if a number is even or odd
+let num = 4;
+if (num % 2 === 0) {
+    console.log("Even number");
+} else {
+    console.log("Odd number");
+}
+
+// 8. Print numbers from 1 to 5
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+// 9. Find the larger number
+let x = 10;
+let y = 7;
+console.log("Greater number:", x > y ? x : y);
+
+// 10. Store and print your name
+let name = "Raj";
+console.log("My name is", name);
